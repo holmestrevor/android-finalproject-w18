ef574c910363aa19c44f375b28ff69ce704c1015
holmestrevor <holmes.trevor98@gmail.com>
2019-03-12 16:42:27 -0400

    MainActivity now links to each activity stub.

--- app/src/main/java/com/example/androidfinalprojectw18/MainActivity.java
+++ app/src/main/java/com/example/androidfinalprojectw18/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.androidfinalprojectw18;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Launches one of the four activities, based on what button was pressed.
+     * @param view - The button being pressed
+     */
+    public void launchActivity(View view) {
+        Button b = (Button)view;
+        Intent i;
+        switch(b.getId()) {
+            case R.id.websterButton:
+                i = new Intent(this, MerriamWebsterDictionary.class);
+                break;
+            case R.id.newsFeedButton:
+                i = new Intent(this, NewsFeed.class);
+                break;
+            case R.id.flightStatusButton:
+                i = new Intent(this, FlightStatusTracker.class);
+                break;
+            case R.id.articleSearchButton:
+                i = new Intent(this, ArticleSearchNYT.class);
+                break;
+            default:
+                i = new Intent(this, MerriamWebsterDictionary.class);
+        }
+        startActivity(i);
     }
 }
