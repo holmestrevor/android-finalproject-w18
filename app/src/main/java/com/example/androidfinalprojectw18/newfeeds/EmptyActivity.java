09fffe9202817c2f67edb82328e2bbff2f362925
Phuc Nguyen <kufuc2604@gmail.com>
2019-04-15 21:26:45 -0400

    News feed: first commit

---
+++ app/src/main/java/com/example/androidfinalprojectw18/newfeeds/EmptyActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidfinalprojectw18.newfeeds;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.androidfinalprojectw18.R;
+
+public class EmptyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newsfeed_empty_activity);
+
+        Bundle dataToPass = getIntent().getExtras(); //get the data that was passed from FragmentExample
+
+        //This is copied directly from FragmentExample.java lines 47-54
+        DetailFragment dFragment = new DetailFragment();
+        dFragment.setArguments( dataToPass ); //pass data to the the fragment
+        dFragment.setTablet(false); //tell the Fragment that it's on a phone.
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragmentLocation, dFragment)
+                .addToBackStack("AnyName")
+                .commit();
+    }
+}
